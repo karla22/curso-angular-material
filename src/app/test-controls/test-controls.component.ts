5c0244263e70926f36fbaceac83b4dbf68cf64a8
luixaviles <luis.avilesz@gmail.com>
2017-01-20 20:11:06 -0400

    Adding test control components

---
+++ src/app/test-controls/test-controls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-controls',
+  templateUrl: './test-controls.component.html',
+  styleUrls: ['./test-controls.component.css']
+})
+export class TestControlsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
